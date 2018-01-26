1b08b1b60cf029fe0907f1b4287c58b3d0993f5b
Harsha Thirimanna <harsha.thirimanna@gmail.com>
2018-01-27 00:13:51 +0530

    Init

---
+++ demo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z "$JAVA_HOME" ]; then
+  echo "You must set the JAVA_HOME variable before running the client."
+  exit 1
+fi
+tmppath=$CLASSPATH
+export CLASSPATH=$CLASSPATH:org.wso2.carbon.demosuite-1.0.0-jar-with-dependencies.jar
+$JAVA_HOME/bin/java org.wso2.carbon.solution.SolutionInstallationApplication $@
+
+
+#running tomcat server that is zipped with the resources.
+cd demo-resources/servers/tomcat/apache-tomcat-9.0.1/bin
+sh ./catalina.sh run
